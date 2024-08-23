828c2356b32498e94d7660e01278af40a26a4f7f
xosadmin <9164356+xosadmin@users.noreply.github.com>
2024-08-23 16:40:50 +0800

    Introduce TLS connection for mail sending function

--- mailsend.py
+++ mailsend.py
@@ -5,18 +5,17 @@ from utils import readConf
 
 def readFile(path):
     try:
-        r = open(path, "r")
-        content = r.readlines()
-        r.close()
+        with open(path, "r") as r:
+            content = r.read()
         return content
     except Exception as e:
         print(e)
         return None
 
-def sendmail(receiver,subject,contentTempPath):
-    sender_email = readConf("mail","senderMail")
+def sendmail(receiver, subject, contentTempPath):
+    sender_email = readConf("mail", "senderMail")
     receiver_email = receiver
-    password = readConf("mail","smtpPassword")
+    password = readConf("mail", "smtpPassword")
 
     message = MIMEMultipart("alternative")
     message["Subject"] = subject
@@ -24,19 +23,27 @@ def sendmail(receiver,subject,contentTempPath):
     message["To"] = receiver_email
 
     mailContent = readFile(contentTempPath)
+    if mailContent is None:
+        print("Cannot find the template")
+        return 1
+
     part = MIMEText(mailContent, "html")
     message.attach(part)
 
     try:
-        smtpServer = readConf("mail","smtpServer")
-        smtpPort = int(readConf("mail","smtpPort"))
+        smtpServer = readConf("mail", "smtpServer")
+        smtpPort = int(readConf("mail", "smtpPort"))
+        ifTls = readConf("mail", "useSSL").lower() == "true"
+
         server = smtplib.SMTP(smtpServer, smtpPort)
-        server.starttls()
+        if ifTls:
+            server.starttls()
         server.login(sender_email, password)
         server.sendmail(sender_email, receiver_email, message.as_string())
+        print("Mail sent success")
         return 0
     except Exception as e:
-        print(e)
+        print(f"Cannot send email: {e}")
         return 1
     finally:
         server.quit()
