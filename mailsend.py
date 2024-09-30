11b17c21ac2ea8b2e4f8fa783b756112875355ed
xosadmin <9164356+xosadmin@users.noreply.github.com>
2024-09-30 14:38:05 +0800

    Add SMTP ssl algorithm

--- mailsend.py
+++ mailsend.py
@@ -77,20 +77,28 @@ def sendmail(receiver, subject, contentTempPath):
     part = MIMEText(mailContent, "html")
     message.attach(part)
 
+    server = None
     try:
         smtpServer = readConf("mail", "smtpServer")
         smtpPort = int(readConf("mail", "smtpPort"))
-        ifTls = readConf("mail", "useSSL").lower() == "true"
+        useSSL = readConf("mail", "useSSL").lower() == "true"
 
-        server = smtplib.SMTP(smtpServer, smtpPort)
-        if ifTls:
+        if useSSL:
+            server = smtplib.SMTP_SSL(smtpServer, smtpPort)
+        else:
+            server = smtplib.SMTP(smtpServer, smtpPort)
+            server.ehlo()
             server.starttls()
+            server.ehlo()
+
         server.login(sender_email, password)
+
         server.sendmail(sender_email, receiver_email, message.as_string())
-        print("Mail sent success")
+        print("Mail sent successfully")
         return 0
     except Exception as e:
         print(f"Cannot send email: {e}")
         return 1
     finally:
-        server.quit()
+        if server is not None:
+            server.quit()
